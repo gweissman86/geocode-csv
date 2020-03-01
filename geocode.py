ed224850dad3734bb667051d0ac3cfee8926a313
Gideon Weissman <gideon.weissman@gmail.com>
2020-03-01 17:12:08 -0500

    now recreates whole csv

--- geocode.py
+++ geocode.py
@@ -6,59 +6,66 @@ import csv, requests, easygui, os, time, pdb
 #apiKey = input('What\'s your Google Maps API key?')
 apiKey = 'AIzaSyCmwWaMtacmFN3NZ9UeKHvtaw0TA52yBJM'
 mapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-locationsPath = ''
+location_path = ''
+location_column = ''
 locations = []
 locationsGeo = []
+new_csv_list = []
 
 def getCsv():
-    global locationsPath
+    global location_path
     input('Select a csv with location names in one column. Press Enter when ready.')
-    locationsPath = easygui.fileopenbox()
-    if '.csv' not in locationsPath:
+    location_path = easygui.fileopenbox()
+    if '.csv' not in location_path:
         input('That\'s not a csv file. Press enter to try again.')
         getCsv()
 
 getCsv()
 
-# pdb.set_trace()
-
-with open(locationsPath, newline='') as csvfile:
+with open(location_path, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    locationColumn = easygui.choicebox(msg = 'Which column are locations in?',
+    location_column = easygui.choicebox(msg = 'Which column are locations in?',
                                    title = 'Select column',
                                    choices = reader.fieldnames)
-    for row in reader:
-        locations.append(row[locationColumn])
-    print('Locations are in column "' + locationColumn + '." The first locations listed are:')
-
-    for i in range(0, min(5,len(locations))):
-        print(locations[i])
-    print('...')
+    print('Locations are in column "' +
+          location_column + '." The first locations listed are:')
+    for i in range(0, 5):
+        try:
+            place = reader.__next__()
+            print(place[location_column])
+        except:
+            break
 
 input('Want to continue?')
 
-for location in locations:
-    try:
-        r = requests.get(mapsUrl + location + '&key=' + apiKey)
-        rjson = r.json()
-        latlng = rjson['results'][0]['geometry']['location']
-        lat = latlng['lat']
-        lng = latlng['lng']
-        locationsGeo.append([location, lat, lng])
-        print('Coordinates added for: ' + location)
-    except:
-        print('ERROR: Something went wrong geocoding ' + location + '. Press Enter to continue.')
-        locationsGeo.append([location, 'Error', 'Error'])
+with open(location_path, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        location = row[location_column]
+        new_row = row
+        try:
+            r = requests.get(mapsUrl + location + '&key=' + apiKey)
+            rjson = r.json()
+            latlng = rjson['results'][0]['geometry']['location']
+            lat = latlng['lat']
+            lng = latlng['lng']
+            new_row['lat'] = lat
+            new_row['lng'] = lng
+            new_csv_list.append(new_row)
+            print('Coordinates added for: ' + location)
+        except:
+            print('ERROR: Something went wrong geocoding ' + location + '. Press Enter to continue.')
+            locationsGeo.append([location, 'Error', 'Error'])
 
-geoCsvPath = os.path.basename(locationsPath)
+geoCsvPath = os.path.basename(location_path)
 geoCsvPath = os.path.splitext(geoCsvPath)[0]
 geoCsvPath = geoCsvPath + '_geocoded_' + time.strftime("%Y%m%d-%H%M%S") + '.csv'
 
 with open(geoCsvPath, 'w', newline='') as csvfile:
-    fieldnames = [locationColumn, 'lat', 'lng']
+    fieldnames = list(new_csv_list[0].keys())
     writer = csv.DictWriter(csvfile, fieldnames)
     writer.writeheader()
-    for i in locationsGeo:
-        writer.writerow({locationColumn: i[0], 'lat': i[1], 'lng': i[2]})
+    for i in new_csv_list:
+        writer.writerow(i)
 
 input('Geodata saved as: ' + geoCsvPath)
